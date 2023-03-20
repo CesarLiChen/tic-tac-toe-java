7be036b88b0e20860fe144c58e530c18adef2e81
Cesar Li Chen <cesarli619@gmail.com>
2023-03-20 10:10:02 -0400

    Starter comments only

---
+++ src/ticTacToe/TicTacToe.java
@@ -0,0 +1,17 @@
+package ticTacToe;
+
+/* Tic Tac Toe 
+ * without using multidimensional arrays
+ * or functions */
+
+public class TicTacToe {
+
+	public static void main(String[] args) {
+		System.out.println("========================");
+		System.out.println("|| A Tic Tac Toe game ||");
+		System.out.println("========================");
+		
+		
+	}
+
+}
