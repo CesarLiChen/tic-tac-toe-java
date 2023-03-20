f67daf0add0fa7fccb4976ed8bc2ade816682304
Cesar Li Chen <cesarli619@gmail.com>
2023-03-20 10:28:34 -0400

    Start of printing the board

--- src/ticTacToe/TicTacToe.java
+++ src/ticTacToe/TicTacToe.java
@@ -5,13 +5,28 @@ package ticTacToe;
  * or functions */
 
 public class TicTacToe {
-
+	
+	final String PLAYER_1 = "O";
+	final String PLAYER_2 = "X";
+	
 	public static void main(String[] args) {
 		System.out.println("========================");
 		System.out.println("|| A Tic Tac Toe game ||");
 		System.out.println("========================");
 		
+		boolean noWinner = true;
 		
+		String[] row1 = {"1", "2", "3"};
+		String[] row2 = {"4", "5", "6"};
+		String[] row3 = {"7", "8", "9"};
+		
+		for (int i = 0; i < row1.length; i++) {
+			System.out.print(row1[i]);
+			if (i == 0 || i == 1) {
+				System.out.print("|");
+			}
+		}
+		System.out.println("\n-----");
 	}
 
 }
