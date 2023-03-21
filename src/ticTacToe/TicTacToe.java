7a48545147703e7fc6386a65c96e4ccd4deff183
Cesar Li Chen <cesarli619@gmail.com>
2023-03-20 21:46:12 -0400

    Some output eye candy, and start of turns taking

--- src/ticTacToe/TicTacToe.java
+++ src/ticTacToe/TicTacToe.java
@@ -1,46 +1,61 @@
 package ticTacToe;
 
+import java.util.Scanner;
+
 /* Tic Tac Toe 
  * without using multidimensional arrays
  * or functions */
 
 public class TicTacToe {
 	
-	final String PLAYER_1 = "O";
-	final String PLAYER_2 = "X";
+	final static String PLAYER_1 = "X";
+	final static String PLAYER_2 = "O";
 	
 	public static void main(String[] args) {
 		System.out.println("========================");
 		System.out.println("|| A Tic Tac Toe game ||");
 		System.out.println("========================\n\n");
 		
-		boolean noWinner = true;
 		
 		String[] row1 = {"1", "2", "3"};
 		String[] row2 = {"4", "5", "6"};
 		String[] row3 = {"7", "8", "9"};
 		
-		for (int i = 0; i < row1.length; i++) {
-			System.out.print(row1[i]);
-			if (i == 0 || i == 1) {
-				System.out.print("|");
-			}
-		}
-		System.out.println("\n-----");
+		Scanner sc = new Scanner(System.in); 
+		String playerTurn = PLAYER_1; // X or O based on turn
+		int turns = 1; //Total of 9 turns
+		boolean noWinner = true;
 		
-		for (int i = 0; i < row2.length; i++) {
-			System.out.print(row2[i]);
-			if (i == 0 || i == 1) {
-				System.out.print("|");
+		System.out.println("X goes first\n");
+		while (turns < 9 || noWinner) {
+			
+			for (int i = 0; i < row1.length; i++) {
+				System.out.print(row1[i]);
+				if (i == 0 || i == 1) {
+					System.out.print("|");
+				}
 			}
-		}
-		System.out.println("\n-----");
-		
-		for (int i = 0; i < row3.length; i++) {
-			System.out.print(row3[i]);
-			if (i == 0 || i == 1) {
-				System.out.print("|");
+			System.out.println("\n-----");
+			
+			for (int i = 0; i < row2.length; i++) {
+				System.out.print(row2[i]);
+				if (i == 0 || i == 1) {
+					System.out.print("|");
+				}
 			}
+			System.out.println("\n-----");
+			
+			for (int i = 0; i < row3.length; i++) {
+				System.out.print(row3[i]);
+				if (i == 0 || i == 1) {
+					System.out.print("|");
+				}
+			}
+			System.out.println("\n\n");
+			
+			System.out.println(playerTurn + ", please write your desired position (1-9)");
+			System.out.print("Input # here: ");
+			int playerInput = sc.nextInt();
 		}
 	}
 
